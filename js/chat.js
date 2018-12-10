40b22c81c7e2c607e88f3c8a6608528bd70d735c
kiriki1991 <kirillmerkulov@tut.by>
2018-12-10 12:11:43 +0300

    + server.js

--- js/chat.js
+++ js/chat.js
@@ -11,125 +11,116 @@ function checkImg(tiss) {
 	})
 }
 
-! function () {
-	var ws = new WebSocket('ws://localhost:3771');
+/*  on ready  */
 
-	/*	on open  */
+document.onreadystatechange = function () {
+	if (document.readyState === "complete") {
+		console.log('<f> doc ready');
+		let modalInit = new Modal(document.getElementById('modalInit'));
+		document.querySelector('.enter-img').addEventListener('click', modalInit.show); // login button
+		var $img = document.querySelector('#avUrl'),
+			$name = document.querySelector('#name'),
+			$message = document.querySelector('#clientMsg');
 
-	ws.onopen = function () {
-		console.log('connection established');
-	}
+		/*  login  */
 
-	/*	on error  */
+		document.querySelector('#logIn').addEventListener('click', function () {
+			if ($name.value !== '') {
 
-	ws.onerror = function (event) {
-		console.log('Ошибка', event);
-		alert('Сервер ' + event.target.url + ' не отвечает');
-	}
+				var ws = new WebSocket('ws://localhost:4815');
 
-	/*	on close  */
+				/*	on open  */
 
-	ws.onclose = function (event) {
-		console.log('Соединение прервано', event);
-		alert('Соединение ' + event.target.url + ' прервано');
-	}
-
-	/*	on message  */
-
-	ws.onmessage = function () {
-		if (event.data !== 'init-chat') {
-			var data = JSON.parse(event.data),
-				$div = document.createElement('div'),
-				$lastMsg = document.createElement('div');
-			$lastMsg.id = 'last-msg';
-			document.querySelector('#last-msg').remove();
-			if (data.name == document.querySelector('#name').value) {
-				$div.classList = 'message user-message';
-			} else {
-				console.log('message');
-				$div.className = 'message';
-			}
-			$div.innerHTML = `<div class="card">
-					<div class="card-body">
-						<div class="user">
-							<img src="` + data.img + `" class="image"></div>
-						<div class="post">
-							<div class="name">` + data.name + ` <div class="time">
-								<span class="badge badge-info">` + data.time + `</span>
-							</div>
-							</div>
-							<div class="text">` + data.message + `</div>
-						</div>
-					</div>
-				</div>`;
-			document.querySelector('main').appendChild($div);
-			document.querySelector('main').appendChild($lastMsg);
-			document.querySelector('#last-msg').scrollIntoView();
-		}
-	};
-
-	/*	on ready  */
-
-	document.onreadystatechange = function () {
-		if (document.readyState === "complete") {
-			console.log('<f> doc ready');
-			let modalInit = new Modal(document.getElementById('modalInit'));
-			document.querySelector('.enter-img').addEventListener('click', modalInit.show);
-			var $img = document.querySelector('#avUrl'),
-				$name = document.querySelector('#name'),
-				$message = document.querySelector('#clientMsg');
-
-			document.querySelector('#logIn').addEventListener('click', function () {
-				if ($name.value !== '') {
-					document.querySelector('.intro').style.display = 'none';
-					document.querySelector('#modalInit .close').click();
-					document.querySelector('#container').style.filter = 'none';
-					document.querySelector('.fixed-bottom').style.display = 'flex';
-					document.querySelector('.fixed-top').style.display = 'flex';
-				} else {
-					document.querySelector('#name').classList = 'form-control mr-sm-2 is-invalid';
+				ws.onopen = function () {
+					console.log('WS connection established!', 'try to log in...');
+					var loginData = {
+						type: 'auth',
+						name: $name.value,
+						img: chosenImg
+					};
+					ws.send(JSON.stringify(loginData));
+					console.log('loginData send...', JSON.stringify(loginData));
 				}
-			})
 
+				/*	on error  */
+
+				ws.onerror = function (event) {
+					console.log('Ошибка', event);
+					alert('Сервер ' + event.target.url + ' не отвечает');
+				}
+
+				/*	on close  */
+
+				ws.onclose = function (event) {
+					console.log('Соединение прервано', event);
+					alert('Соединение ' + event.target.url + ' прервано');
+				}
+
+				/*  on message  */
+
+				ws.onmessage = function () {
+					console.log('<incmgMSG> type:', JSON.parse(event.data).type);
+					if (JSON.parse(event.data).type == 'message') {
+						var data = JSON.parse(event.data),
+							$div = document.createElement('div'),
+							$lastMsg = document.createElement('div');
+						$lastMsg.id = 'last-msg';
+						document.querySelector('#last-msg').remove();
+						if (data.name == document.querySelector('#name').value) {
+							$div.classList = 'message user-message';
+						} else {
+							$div.className = 'message';
+						}
+						$div.innerHTML = `<div class="card">
+							<div class="card-body">
+								<div class="user">
+									<img src="` + data.img + `" class="image"></div>
+								<div class="post">
+									<div class="name">` + data.name + ` <div class="time">
+										<span class="badge badge-info">` + data.time + `</span>
+									</div>
+									</div>
+									<div class="text">` + data.message + `</div>
+								</div>
+							</div>
+						</div>`;
+						document.querySelector('main').appendChild($div);
+						document.querySelector('main').appendChild($lastMsg);
+						document.querySelector('#last-msg').scrollIntoView();
+					} else if (JSON.parse(event.data).type == 'auth') {
+						if (JSON.parse(event.data).id == 'error') {
+							console.error('auth fail!');
+						} else {
+							console.log('success auth!');
+							document.querySelector('.intro').style.display = 'none';
+							document.querySelector('#modalInit .close').click();
+							document.querySelector('#container').style.filter = 'none';
+							document.querySelector('.fixed-bottom').style.display = 'flex';
+							document.querySelector('.fixed-top').style.display = 'flex';
+						}
+					}
+				}
+			} else {
+				document.querySelector('#name').classList = 'form-control mr-sm-2 is-invalid';
+			}
 			document.querySelector('#send').addEventListener('click', function () {
 				if (chosenImg == '') chosenImg = 'img/0.jpg';
 				if ($message.value == '') return;
-				var name = $name.value,
-					message = $message.value,
-					time = new Date().toLocaleTimeString();
 				data = {
-					name: name,
+					type: 'message',
+					name: $name.value,
 					img: chosenImg,
-					message: message,
-					time: time
+					message: $message.value,
+					time: new Date().toLocaleTimeString()
 				};
 				$message.value = '';
-				/*try {
-					ws.send(JSON.stringify(data));
-					throw new Error('Сообщение не отправлено: Сервер не отвечает');
-				} catch (e) {
-					if (e) {
-						console.log(e);
-					} else {
-						console.log(data);
-						document.querySelector('#clientMsg').value = '';
-					}
-				}*/
-
-				ws.send(JSON.stringify(data), function ack(error) {
-					alert(error);
-				});
-				//ws.send(JSON.stringify(data));
-
+				ws.send(JSON.stringify(data));
 			})
-
 			document.querySelector('#clientMsg').addEventListener('keypress', function (event) {
 				if (event.keyCode == 13) {
 					document.querySelector('#send').click();
 				}
-			});
-
-		}
+			})
+		})
 	}
-
-}()
+}
---
+++ server/server.js
@@ -0,0 +1,97 @@
+var WebSocketServer = require('ws').Server,
+	fs = require('fs'),
+	usersOfTable1 = {},
+	i,
+	wss = new WebSocketServer({
+		port: 4815
+	});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	})
+}
+
+/*  on connection  */
+
+wss.on('connection', function (ws) {
+	ws.on('message', function incoming(message) {
+		if (JSON.parse(message).type == 'auth') {
+			var id = Math.random(), // ID generating
+				trig = false; // search trigger
+			usersOfTable1[id] = ws; // temp save new client as obj
+			console.log('<newConnection> id:' + id);
+			authData = { // preparing logIn obj
+				type: 'auth',
+				img: JSON.parse(message).img,
+				name: toHtml(JSON.parse(message).name.toLowerCase()),
+				id: id,
+				client: usersOfTable1[id],
+				time: new Date().toLocaleTimeString()
+			};
+			Object.keys(usersOfTable1).forEach(function (item) {
+				if (authData.name == usersOfTable1[item].name) {
+					console.log('<error> user "' + usersOfTable1[item].name + '" already connected');
+					trig = true;
+					return;
+				}
+			})
+			if (!trig) {
+				usersOfTable1[id].send(JSON.stringify(authData)); // send logIn response to user
+				usersOfTable1[id] = authData; // replace wsObj to responseObj
+				showOnlineUsers() // debug logs start
+				ws.on('close', function () { // on close ws connection
+					console.log('<connection lost> ' + id);
+					delete usersOfTable1[id];
+					showOnlineUsers(); // debug logs start
+				});
+			} else {
+				authData = { // preparing logIn obj
+					type: 'auth',
+					id: 'error'
+				};
+				usersOfTable1[id].send(JSON.stringify(authData)); // send logIn response to user
+				usersOfTable1[id].close();
+				delete usersOfTable1[id];
+			}
+		} else if (JSON.parse(message).type == 'message') {
+			message = JSON.parse(message);
+			message.message = toHtml(message.message);
+			console.log('<newMsg>', message);
+			wss.clients.forEach(function each(client) {
+				client.send(JSON.stringify(message));
+			})
+		}
+	})
+})
+
+function showOnlineUsers() { /*  logs  */
+	console.log('');
+	console.log('--------------users-table---------------');
+	fs.appendFileSync('log.txt', '\r\n---------[' + new Date().toLocaleTimeString() + ']users-table---------', 'ascii');
+	let i = 0;
+	Object.keys(usersOfTable1).forEach(function (item) {
+		i++;
+		console.log('#' + i + ' | name: "' + usersOfTable1[item].name + '" | id: ' + usersOfTable1[item].id);
+		fs.appendFileSync('log.txt', '\r\n#' + i + ' | name: "' + usersOfTable1[item].name + '" | id: ' + usersOfTable1[item].id, 'ascii');
+	});
+	console.log('----------------------------------------');
+	fs.appendFileSync('log.txt', '\r\n----------------------------------------', 'ascii');
+	console.log('');
+}
+
+function toHtml(string) {
+	let entityMap = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#39;',
+		'/': '&#x2F;',
+		'`': '&#x60;',
+		'=': '&#x3D;'
+	}
+	return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
